64b8eff3d9e25d66414a49c088a67fc1ec9b7950
Erik Wessman <erikwessman10@gmail.com>
2023-02-26 21:22:16 +0100

    Added some test data and methods

--- src/main.py
+++ src/main.py
@@ -1,7 +1,29 @@
 from flask import Flask
+from flask import request
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def start():
+    return "OK", 200
+
+@app.route("/article", methods=["GET"])
+def get_articles():
+    f = open("src/test_data/articles.json")
+    articles = json.load(f)
+    f.close()
+    return articles, 200
+
+@app.route("/article<id>", methods=["GET"])
+def get_article(id):
+    return "OK", 200
+
+@app.route("/article<id>", methods=["PATCH"])
+def patch_article(id):
+    return "OK", 200
+
+@app.route("/article", methods=["POST"])
+def create_articles():
+    article = request.data
+    return "OK", 200
