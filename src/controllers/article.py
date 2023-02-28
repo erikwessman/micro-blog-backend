2a9c0ec6a5fadbb6bd9179ed50b6c6417f70f539
Erik Wessman <erikwessman10@gmail.com>
2023-02-28 23:18:35 +0100

    Implemented patch, delete and get for articles

--- src/controllers/article.py
+++ src/controllers/article.py
@@ -1,16 +1,30 @@
 import json
 from db import DBManager
 from utils.json_encoder import JSONEncoder
-from flask import request, jsonify, Blueprint
+from flask import request, Blueprint
+from bson import ObjectId
+import bson
 
 article_bp = Blueprint('article_route', __name__, url_prefix='/api/article', template_folder='templates')
 article_db = DBManager.get_db()['articles']
 
-@article_bp.route("", methods=["GET"])
+
+@article_bp.route("/all", methods=["GET"])
 def get_articles():
     articles = list(article_db.find())
     articles_json = JSONEncoder().encode(articles)
+
     return articles_json, 200
+
+
+@article_bp.route("", methods=["GET"])
+def get_article():
+    article_id = request.args['id']
+    article = article_db.find_one({"_id": ObjectId(article_id)})
+    article_json = JSONEncoder().encode(article)
+
+    return article_json, 200
+
 
 @article_bp.route("", methods=["POST"])
 def create_articles():
@@ -19,16 +33,24 @@ def create_articles():
 
     article_insert = article_db.insert_one(article)
     article_id = str(article_insert.inserted_id)
+
     return article_id, 200
 
-@article_bp.route("<id>", methods=["GET"])
-def get_article(id):
+
+@article_bp.route("", methods=["PATCH"])
+def patch_article():
+    article_json = request.data
+    article = json.loads(article_json)
+    article_id = request.args['id']
+
+    article_db.update_one({"_id": ObjectId(article_id)},
+                          {"$set": article})
     return "OK", 200
 
-@article_bp.route("<id>", methods=["PATCH"])
-def patch_article(id):
-    return "OK", 200
 
-@article_bp.route("<id>", methods=["DELETE"])
-def delete_article(id):
+@article_bp.route("", methods=["DELETE"])
+def delete_article():
+    article_id = request.args['id']
+    article_db.delete_one({"_id": ObjectId(article_id)})
+
     return "OK", 200
