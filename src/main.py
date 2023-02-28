8f368b23c425b4cde5f374108fb634ddb9e4e953
Erik Wessman <erikwessman10@gmail.com>
2023-02-28 23:52:51 +0100

    Enabled CORS for API endpoints

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from controllers.article import article_bp
 
 # Create Flask app and connect routes/Blueprints
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(article_bp)
 
 
