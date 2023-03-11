0eeebd534e640d4e747be3632a0220ce28a838ca
Erik Wessman <erikwessman10@gmail.com>
2023-03-11 21:11:49 +0100

    Added custom user article endpoint

--- src/controllers/article.py
+++ src/controllers/article.py
@@ -1,9 +1,10 @@
 import json
-from datetime import datetime
 from db import DBManager
 from utils.json_encoder import JSONEncoder
-from flask import request, Blueprint
+from flask import request, Blueprint, current_app
 from bson import ObjectId
+import jwt
+import datetime
 
 article_bp = Blueprint('article_route', __name__,
                        url_prefix='/api/article', template_folder='templates')
@@ -19,7 +20,7 @@ def get_article():
         article = article_db.find_one({"_id": ObjectId(article_id)})
     else:
         article = list(article_db.find(request.args))
-        article.sort(key=lambda x: datetime.strptime(x['date'], '%d-%m-%Y'), reverse=True)
+        article.sort(key=lambda x: datetime.datetime.strptime(x['date'], '%d-%m-%Y'), reverse=True)
 
     article_json = JSONEncoder().encode(article)
     return article_json, 200
@@ -36,13 +37,31 @@ def create_article():
     return article_id, 200
 
 
+@article_bp.route("/user", methods=["POST"])
+def create_article_user():
+    article_json = request.data
+    article = json.loads(article_json)
+    
+    token = request.headers.get('Authorization')
+    data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=["HS256"])
+    username = data['username']
+
+    article['author'] = username
+    article['date'] = datetime.datetime.today().strftime('%d-%m-%Y')
+
+    article_insert = article_db.insert_one(article)
+    article_id = str(article_insert.inserted_id)
+
+    return article_id, 200
+
+
 @article_bp.route("/dummy", methods=["POST"])
 def create_article_dummy():
     f = open("src/dummy_data/article.json")
     article = json.load(f)
     f.close()
 
-    article['date'] = datetime.today().strftime('%d-%m-%Y')
+    article['date'] = datetime.datetime.today().strftime('%d-%m-%Y')
 
     article_insert = article_db.insert_one(article)
     article_id = str(article_insert.inserted_id)
